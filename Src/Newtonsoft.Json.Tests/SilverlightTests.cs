a6662b595dbc316c6e7aa8ec1ac262dd68a7ffe4
JamesNK <james@newtonking.com>
2008-08-23 02:10:53 +0000

    -Include SilverlightTests.cs

---
+++ Src/Newtonsoft.Json.Tests/SilverlightTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace Newtonsoft.Json.Tests
+{
+#if SILVERLIGHT
+  [TestFixture]
+  public class SilverlightTests
+  {
+    [Test]
+    public void SystemVersion()
+    {
+      Assembly systemAssembly = typeof(Uri).Assembly;
+      StringAssert.Contains("=2.0.5.0,", systemAssembly.FullName,
+          "Check we're testing a Silverlight 2.0 assembly");
+    }
+  }
+#endif
+}
