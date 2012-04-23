6268d00a0d582cc314cc1c6971e060e6ab5f0f82
James Newton-King <james@newtonking.com>
2012-04-23 20:59:35 +1200

    -Increment version to 4.5.4

--- Build/build.ps1
+++ Build/build.ps1
@@ -1,7 +1,7 @@
 ﻿properties { 
-  $zipFileName = "Json45r3.zip"
+  $zipFileName = "Json45r4.zip"
   $majorVersion = "4.5"
-  $majorWithReleaseVersion = "4.5.3"
+  $majorWithReleaseVersion = "4.5.4"
   $version = GetVersion $majorWithReleaseVersion
   $signAssemblies = $false
   $signKeyPath = "D:\Development\Releases\newtonsoft.snk"
