06a139f0b9c0646ca8bd10039b6e70b985d0eeed
Anthony Johnston <aj@antix.co.uk>
2013-05-31 12:08:06 +0100

    JToken.Merge extensions added
    Merge to create a new token which is the merge of two tokens
    MergeInto to merge values into an existing token
    Both overwrite the left with the right

---
+++ Src/Newtonsoft.Json.Tests/Linq/MergeExtensionsTest.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+
+using Newtonsoft.Json.Linq;
+
+namespace Newtonsoft.Json.Tests.Linq
+{
+    [TestFixture]
+    public class MergeExtensionsTest
+    {
+        [Test]
+        public void can_merge_a_property()
+        {
+            var left = JToken.FromObject(new {Property1 = 1});
+            var right = JToken.FromObject(new {Property2 = 2});
+
+           var result = left.Merge(right);
+
+           Assert.AreEqual("{\"Property1\":1,\"Property2\":2}", result.ToString(Formatting.None));
+        }
+
+        [Test]
+        public void can_merge_a_property_over_a_value()
+        {
+            var left = JToken.FromObject(1);
+            var right = JToken.FromObject(new {Property2 = 2});
+
+            var result = left.Merge(right);
+
+            Assert.AreEqual("{\"Property2\":2}", result.ToString(Formatting.None));
+        }
+
+        [Test]
+        public void can_merge_a_sub_property()
+        {
+            var left = JToken.FromObject(new { Property1 = new { SubProperty1 = 1 } });
+            var right = JToken.FromObject(new { Property1 = new { SubProperty2 = 2 } });
+
+            var result = left.Merge(right);
+
+            Assert.AreEqual("{\"Property1\":{\"SubProperty1\":1,\"SubProperty2\":2}}", result.ToString(Formatting.None));
+        }
+
+        [Test]
+        public void can_merge_properties_and_a_sub_properties()
+        {
+            var left = JToken.FromObject(new { Property1 = new { SubProperty1 = 1 } });
+            var right = JToken.FromObject(new { Property1 = new { SubProperty2 = 2 }, Property2 = 2 });
+
+            var result = left.Merge(right);
+
+            Assert.AreEqual("{\"Property1\":{\"SubProperty1\":1,\"SubProperty2\":2},\"Property2\":2}", result.ToString(Formatting.None));
+        }
+
+        [Test]
+        public void can_merge_objects_with_arrays_by_overwriting()
+        {
+            var left = JToken.FromObject(new
+            {
+                Array1 = new object[]
+                        {
+                            new {Property1 = 1}
+                        }
+            });
+            var right = JToken.FromObject(new
+            {
+                Array1 = new object[]
+                        {
+                            new {Property2 = 2},
+                            new {Property3 = 3}
+                        }
+            });
+
+            var result = left.Merge(right);
+
+            Assert.AreEqual("{\"Array1\":[{\"Property2\":2},{\"Property3\":3}]}", result.ToString(Formatting.None));
+        }
+    }
+}
---
+++ Src/Newtonsoft.Json/Linq/MergeExtensions.cs
@@ -0,0 +1,61 @@
+﻿namespace Newtonsoft.Json.Linq
+{
+    /// <summary>
+    /// <para>Extensions for merging JTokens</para>
+    /// </summary>
+    public static class MergeExtensions
+    {
+        /// <summary>
+        /// <para>Creates a new token which is the merge of the passed tokens</para>
+        /// </summary>
+        /// <param name="left">Token</param>
+        /// <param name="right">Token to merge, overwriting the left</param>
+        /// <returns>A new merged token</returns>
+        public static JToken Merge(
+            this JToken left, JToken right)
+        {
+            if (left.Type != JTokenType.Object)
+                return right.DeepClone();
+
+            var leftClone = (JContainer)left.DeepClone();
+            MergeInto(leftClone, right);
+
+            return leftClone;
+        }
+
+        /// <summary>
+        /// <para>Merge the right token into the left</para>
+        /// </summary>
+        /// <param name="left">Token to be merged into</param>
+        /// <param name="right">Token to merge, overwriting the left</param>
+        public static void MergeInto(
+            this JContainer left, JToken right)
+        {
+            foreach (var rightChild in right.Children<JProperty>())
+            {
+                var rightChildProperty = rightChild;
+                var leftProperty = left.SelectToken(rightChildProperty.Name);
+
+                if (leftProperty == null)
+                {
+                    // no matching property, just add 
+                    left.Add(rightChild);
+                }
+                else
+                {
+                    var leftObject = leftProperty as JObject;
+                    if (leftObject == null)
+                    {
+                        // replace value
+                        var leftParent = (JProperty) leftProperty.Parent;
+                        leftParent.Value = rightChildProperty.Value;
+                    }
+
+                    else
+                        // recurse object
+                        MergeInto(leftObject, rightChildProperty.Value);
+                }
+            }
+        }
+    }
+}
