cd65c1bdd2df9bd91b43e0a74376c4120e952912
Daniel Halan <danielhalan@gmail.com>
2013-10-11 14:23:59 +0700

    Added ConstructorHandlingFallbackEventArgs to legacy NET projects

--- Src/Newtonsoft.Json/ConstructorHandlingFallbackEventArgs.cs
+++ Src/Newtonsoft.Json/ConstructorHandlingFallbackEventArgs.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json.Serialization;
 
 namespace Newtonsoft.Json {
