2cb89a7a3fd70202782a7233510609ce42f92525
James Newton-King <james@newtonking.com>
2013-10-13 20:31:41 +1300

    -Documentation

--- Src/Newtonsoft.Json/Converters/JsonValueConverter.cs
+++ Src/Newtonsoft.Json/Converters/JsonValueConverter.cs
@@ -33,8 +33,17 @@ using Windows.Data.Json;
 
 namespace Newtonsoft.Json.Converters
 {
+  /// <summary>
+  /// Converts a <see cref="IJsonValue"/> to and from JSON.
+  /// </summary>
   public class JsonValueConverter : JsonConverter
   {
+    /// <summary>
+    /// Writes the JSON representation of the object.
+    /// </summary>
+    /// <param name="writer">The <see cref="JsonWriter"/> to write to.</param>
+    /// <param name="value">The value.</param>
+    /// <param name="serializer">The calling serializer.</param>
     public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
     {
       WriteJsonValue(writer, (IJsonValue)value);
@@ -99,6 +108,14 @@ namespace Newtonsoft.Json.Converters
       }
     }
 
+    /// <summary>
+    /// Reads the JSON representation of the object.
+    /// </summary>
+    /// <param name="reader">The <see cref="JsonReader"/> to read from.</param>
+    /// <param name="objectType">Type of the object.</param>
+    /// <param name="existingValue">The existing value of object being read.</param>
+    /// <param name="serializer">The calling serializer.</param>
+    /// <returns>The object value.</returns>
     public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
     {
       if (reader.TokenType == JsonToken.None)
@@ -191,6 +208,13 @@ namespace Newtonsoft.Json.Converters
       throw JsonSerializationException.Create(reader, "Unexpected end.");
     }
 
+    /// <summary>
+    /// Determines whether this instance can convert the specified object type.
+    /// </summary>
+    /// <param name="objectType">Type of the object.</param>
+    /// <returns>
+    /// 	<c>true</c> if this instance can convert the specified object type; otherwise, <c>false</c>.
+    /// </returns>
     public override bool CanConvert(Type objectType)
     {
       return typeof(IJsonValue).IsAssignableFrom(objectType);
