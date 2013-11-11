b3db255b1da69792b78aebb47aeee0a75a4095ba
Shaun Wilde <shaun_wilde@hotmail.com>
2013-11-12 07:12:02 +1100

    add skip switches to opencover cmd

--- Build/opencover.bat
+++ Build/opencover.bat
@@ -1,3 +1,3 @@
-..\Tools\OpenCover\opencover.console.exe -register:user -target:runtests.bat -filter:"+[Newtonsoft.*]* -[*.Tests]*"
+..\Tools\OpenCover\opencover.console.exe -register:user -target:runtests.bat -filter:"+[Newtonsoft.*]* -[*.Tests]*" -skipautoprops -hideskipped
 
 ..\Tools\ReportGenerator\ReportGenerator.exe -reports:results.xml -targetdir:.\reports
