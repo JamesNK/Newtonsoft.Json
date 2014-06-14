28cd8c497ac824a844cc49a9bb956b69a8d65db8
James Newton-King <james@newtonking.com>
2014-06-15 01:36:10 +1200

    -Derp

--- Src/Newtonsoft.Json/Serialization/JsonSerializerInternalReader.cs
+++ Src/Newtonsoft.Json/Serialization/JsonSerializerInternalReader.cs
@@ -2020,8 +2020,7 @@ To fix this error either change the environment to be fully trusted, change the 
                                     if (property.PropertyContract == null)
                                         property.PropertyContract = GetContractSafe(property.PropertyType);
 
-                                    if (HasFlag(property.DefaultValueHandling.GetValueOrDefault(Serializer._defaultValueHandling), DefaultValueHandling.Populate) && property.Writable && !property.Ignored
-)
+                                    if (HasFlag(property.DefaultValueHandling.GetValueOrDefault(Serializer._defaultValueHandling), DefaultValueHandling.Populate) && property.Writable && !property.Ignored)
                                         property.ValueProvider.SetValue(newObject, EnsureType(reader, property.GetResolvedDefaultValue(), CultureInfo.InvariantCulture, property.PropertyContract, property.PropertyType));
                                     break;
                                 case PropertyPresence.Null:
