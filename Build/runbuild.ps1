a3df7ecd98844b6de4044e2b9b46b7d16104b9da
James Newton-King <james@newtonking.com>
2015-03-21 23:09:27 +1300

    -Build

--- Build/runbuild.ps1
+++ Build/runbuild.ps1
@@ -1,5 +1,7 @@
 ﻿cls
 
-Import-Module '..\Tools\PSake\psake.psm1'
-Invoke-psake '.\build.ps1' Test -framework 3.5
+$path = Split-Path -Path $MyInvocation.MyCommand.Path
+
+Import-Module ($path + '\..\Tools\PSake\psake.psm1')
+Invoke-psake ($path + '\build.ps1') Test -framework 3.5
 Remove-Module psake
