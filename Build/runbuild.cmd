d503779c1858fe3eecd8aa626975ade3123f699f
James Newton-King <james@newtonking.com>
2015-03-21 22:38:06 +1300

    -Build

--- Build/runbuild.cmd
+++ Build/runbuild.cmd
@@ -1,2 +1,2 @@
 cls
-powershell -Command "& { [Console]::WindowWidth = 150; [Console]::WindowHeight = 50; Start-Transcript runbuild.txt; Import-Module ..\Tools\PSake\psake.psm1; Invoke-psake ..\Build\build.ps1 %*; Stop-Transcript; }"
+powershell -Command "& { [Console]::WindowWidth = 150; [Console]::WindowHeight = 50; Start-Transcript %~dp0runbuild.txt; Import-Module %~dp0..\Tools\PSake\psake.psm1; Invoke-psake %~dp0..\Build\build.ps1 %*; Stop-Transcript; }"
