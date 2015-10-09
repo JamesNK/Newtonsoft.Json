11f9f106d41ea7eecfb0835503145717ca55ab7f
devgopher <igevdo@mail.ru>
2015-10-09 14:04:08 +0300

    Mono40 sln

--- Src/Newtonsoft.Json/Properties/AssemblyInfo.cs
+++ Src/Newtonsoft.Json/Properties/AssemblyInfo.cs
@@ -22,7 +22,6 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
 #endregion
-
 using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
@@ -32,55 +31,33 @@ using System.Security;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-#if PORTABLE40
-[assembly: AssemblyTitle("Json.NET Portable .NET 4.0")]
-#elif PORTABLE
-[assembly: AssemblyTitle("Json.NET Portable")]
-#elif DOTNET
-[assembly: AssemblyTitle("Json.NET .NET Platform")]
-#elif NET20
-[assembly: AssemblyTitle("Json.NET .NET 2.0")]
+
+[assembly: AssemblyTitle ("Json.NET")]
 [assembly: AllowPartiallyTrustedCallers]
-#elif NET35
-[assembly: AssemblyTitle("Json.NET .NET 3.5")]
-[assembly: AllowPartiallyTrustedCallers]
-#elif NET40
-[assembly: AssemblyTitle("Json.NET .NET 4.0")]
-[assembly: AllowPartiallyTrustedCallers]
-#else
-[assembly: AssemblyTitle("Json.NET")]
-[assembly: AllowPartiallyTrustedCallers]
-#endif
 
 #if !SIGNED
-[assembly: InternalsVisibleTo("Newtonsoft.Json.Schema")]
-[assembly: InternalsVisibleTo("Newtonsoft.Json.Tests")]
+[assembly: InternalsVisibleTo ("Newtonsoft.Json.Schema")]
+[assembly: InternalsVisibleTo ("Newtonsoft.Json.Tests")]
 #else
 [assembly: InternalsVisibleTo("Newtonsoft.Json.Schema, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f561df277c6c0b497d629032b410cdcf286e537c054724f7ffa0164345f62b3e642029d7a80cc351918955328c4adc8a048823ef90b0cf38ea7db0d729caf2b633c3babe08b0310198c1081995c19029bc675193744eab9d7345b8a67258ec17d112cebdbbb2a281487dceeafb9d83aa930f32103fbe1d2911425bc5744002c7")]
 [assembly: InternalsVisibleTo("Newtonsoft.Json.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f561df277c6c0b497d629032b410cdcf286e537c054724f7ffa0164345f62b3e642029d7a80cc351918955328c4adc8a048823ef90b0cf38ea7db0d729caf2b633c3babe08b0310198c1081995c19029bc675193744eab9d7345b8a67258ec17d112cebdbbb2a281487dceeafb9d83aa930f32103fbe1d2911425bc5744002c7")]
 #endif
-
-[assembly: InternalsVisibleTo("Newtonsoft.Json.Dynamic, PublicKey=0024000004800000940000000602000000240000525341310004000001000100cbd8d53b9d7de30f1f1278f636ec462cf9c254991291e66ebb157a885638a517887633b898ccbcf0d5c5ff7be85a6abe9e765d0ac7cd33c68dac67e7e64530e8222101109f154ab14a941c490ac155cd1d4fcba0fabb49016b4ef28593b015cab5937da31172f03f67d09edda404b88a60023f062ae71d0b2e4438b74cc11dc9")]
-[assembly: AssemblyDescription("Json.NET is a popular high-performance JSON framework for .NET")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Newtonsoft")]
-[assembly: AssemblyProduct("Json.NET")]
-[assembly: AssemblyCopyright("Copyright © James Newton-King 2008")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
+[assembly: InternalsVisibleTo ("Newtonsoft.Json.Dynamic, PublicKey=0024000004800000940000000602000000240000525341310004000001000100cbd8d53b9d7de30f1f1278f636ec462cf9c254991291e66ebb157a885638a517887633b898ccbcf0d5c5ff7be85a6abe9e765d0ac7cd33c68dac67e7e64530e8222101109f154ab14a941c490ac155cd1d4fcba0fabb49016b4ef28593b015cab5937da31172f03f67d09edda404b88a60023f062ae71d0b2e4438b74cc11dc9")]
+[assembly: AssemblyDescription ("Json.NET is a popular high-performance JSON framework for .NET")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Newtonsoft")]
+[assembly: AssemblyProduct ("Json.NET")]
+[assembly: AssemblyCopyright ("Copyright ï¿½ James Newton-King 2008")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
 #if !(PORTABLE40 || PORTABLE)
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM componenets.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
+[assembly: ComVisible (false)]
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("9ca358aa-317b-4925-8ada-4a29e943a363")]
+[assembly: Guid ("9ca358aa-317b-4925-8ada-4a29e943a363")]
 #endif
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -90,7 +67,6 @@ using System.Security;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-
-[assembly: AssemblyVersion("7.0.0.0")]
-[assembly: AssemblyFileVersion("7.0.2.18802")]
-[assembly: CLSCompliant(true)]
+[assembly: AssemblyVersion ("7.0.0.0")]
+[assembly: AssemblyFileVersion ("7.0.2.18802")]
+[assembly: CLSCompliant (true)]
