331fcc0ebb2f5fdc8f4a66673a4074068efe44ef
James Newton-King <james@newtonking.com>
2015-11-23 20:15:37 +1300

    -Added support for 24 hour ISO dates

--- Src/Newtonsoft.Json.Tests/Utilities/DateTimeUtilsTests.cs
+++ Src/Newtonsoft.Json.Tests/Utilities/DateTimeUtilsTests.cs
@@ -71,6 +71,32 @@ namespace Newtonsoft.Json.Tests.Utilities
         }
 
         [Test]
+        public void Parse24HourDateTime()
+        {
+            DateTime dt;
+            Assert.IsTrue(DateTimeUtils.TryParseDateTimeIso(CreateStringReference("2000-12-15T24:00:00Z"), DateTimeZoneHandling.RoundtripKind, out dt));
+            Assert.AreEqual(new DateTime(2000, 12, 16, 0, 0, 0, DateTimeKind.Utc), dt);
+
+            Assert.IsFalse(DateTimeUtils.TryParseDateTimeIso(CreateStringReference("2000-12-15T24:01:00Z"), DateTimeZoneHandling.RoundtripKind, out dt));
+            Assert.IsFalse(DateTimeUtils.TryParseDateTimeIso(CreateStringReference("2000-12-15T24:00:01Z"), DateTimeZoneHandling.RoundtripKind, out dt));
+            Assert.IsFalse(DateTimeUtils.TryParseDateTimeIso(CreateStringReference("2000-12-15T24:00:00.0000001Z"), DateTimeZoneHandling.RoundtripKind, out dt));
+        }
+
+#if !NET20
+        [Test]
+        public void Parse24HourDateTimeOffset()
+        {
+            DateTimeOffset dt;
+            Assert.IsTrue(DateTimeUtils.TryParseDateTimeOffsetIso(CreateStringReference("2000-12-15T24:00:00Z"), out dt));
+            Assert.AreEqual(new DateTimeOffset(2000, 12, 16, 0, 0, 0, TimeSpan.Zero), dt);
+
+            Assert.IsFalse(DateTimeUtils.TryParseDateTimeOffsetIso(CreateStringReference("2000-12-15T24:01:00Z"), out dt));
+            Assert.IsFalse(DateTimeUtils.TryParseDateTimeOffsetIso(CreateStringReference("2000-12-15T24:00:01Z"), out dt));
+            Assert.IsFalse(DateTimeUtils.TryParseDateTimeOffsetIso(CreateStringReference("2000-12-15T24:00:00.0000001Z"), out dt));
+        }
+#endif
+
+        [Test]
         public void NewDateTimeParse()
         {
             AssertNewDateTimeParseEqual("999x-12-31T23:59:59");
