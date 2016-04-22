9127cc4debfd048afa9efe14c97ef1b815d98208
James Newton-King <james@newtonking.com>
2016-04-22 21:05:38 +1200

    -Build

--- Src/Newtonsoft.Json/Properties/AssemblyInfo.cs
+++ Src/Newtonsoft.Json/Properties/AssemblyInfo.cs
@@ -32,7 +32,9 @@ using System.Security;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-#if PORTABLE40
+#if NETSTANDARD10
+[assembly: AssemblyTitle("Json.NET .NET Standard 1.0")]
+#elif PORTABLE40
 [assembly: AssemblyTitle("Json.NET Portable .NET 4.0")]
 #elif PORTABLE
 [assembly: AssemblyTitle("Json.NET Portable")]
@@ -48,7 +50,6 @@ using System.Security;
 [assembly: AssemblyTitle("Json.NET .NET 4.0")]
 [assembly: AllowPartiallyTrustedCallers]
 #else
-
 [assembly: AssemblyTitle("Json.NET")]
 [assembly: AllowPartiallyTrustedCallers]
 #endif
