72c75740505ab69543a3bd228588573fb90ffa2f
Jon Hanna <jon@hackcraft.net>
2017-01-06 13:27:11 +0000

    Async JToken methods

--- Src/Newtonsoft.Json/Linq/JValue.cs
+++ Src/Newtonsoft.Json/Linq/JValue.cs
@@ -41,7 +41,7 @@ namespace Newtonsoft.Json.Linq
     /// <summary>
     /// Represents a value in JSON (string, integer, date, etc).
     /// </summary>
-    public class JValue : JToken, IEquatable<JValue>, IFormattable, IComparable, IComparable<JValue>
+    public partial class JValue : JToken, IEquatable<JValue>, IFormattable, IComparable, IComparable<JValue>
 #if !PORTABLE
         , IConvertible
 #endif
