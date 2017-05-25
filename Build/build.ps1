2b647efc7b7ec815ca20a46c1859c8e48d335d76
James Newton-King <james@newtonking.com>
2017-05-25 19:16:20 +0700

    -Build

--- Build/build.ps1
+++ Build/build.ps1
@@ -11,7 +11,7 @@
   $buildNuGet = $false
   $treatWarningsAsErrors = $false
   $workingName = if ($workingName) {$workingName} else {"Working"}
-  $netCliVersion = "1.0.0"
+  $netCliVersion = "1.0.4"
   $nugetUrl = "http://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
   
   $baseDir  = resolve-path ..
