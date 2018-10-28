e3c4c6aa6b8f24aee41e7a5a193b727f0a57bd11
James Newton-King <james@newtonking.com>
2018-10-28 15:19:10 +1300

    Update build to deploy to NuGet (#1899)

---
+++ Build/localbuild.ps1
@@ -0,0 +1,5 @@
+﻿Start-Transcript ($PSScriptRoot + '\Temp\runbuild.txt')
+
+$version = Get-Content "$PSScriptRoot\version.json" | Out-String | ConvertFrom-Json
+
+& $PSScriptRoot\runbuild.ps1 -properties @{"majorVersion"="$($version.Major).0"; "majorWithReleaseVersion"="$($version.Major).0.$($version.Release)"; "nugetPrerelease"=$version.Prerelease; "zipFileName"="Json$($version.Major)0r$($version.Release).zip"; "treatWarningsAsErrors"=$true}
