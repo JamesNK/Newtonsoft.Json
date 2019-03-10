52e72a9c508829621f52f0e26e389f64f0a5f14e
xp44mm <34696643@qq.com>
2019-03-10 13:09:27 +0800

    Update ReflectionUtils.cs

--- Src/Newtonsoft.Json/Utilities/ReflectionUtils.cs
+++ Src/Newtonsoft.Json/Utilities/ReflectionUtils.cs
@@ -178,7 +178,43 @@ namespace Newtonsoft.Json.Utilities
 
         private static string RemoveAssemblyDetails(string fullyQualifiedTypeName)
         {
-            return SplitFullyQualifiedTypeName(fullyQualifiedTypeName).Value2;
+            StringBuilder builder = new StringBuilder();
+
+            // loop through the type name and filter out qualified assembly details from nested type names
+            bool writingAssemblyName = false;
+            bool skippingAssemblyDetails = false;
+            for (int i = 0; i < fullyQualifiedTypeName.Length; i++)
+            {
+                char current = fullyQualifiedTypeName[i];
+                switch (current)
+                {
+                    case '[':
+                    case ']':
+                        writingAssemblyName = false;
+                        skippingAssemblyDetails = false;
+                        builder.Append(current);
+                        break;
+                    case ',':
+                        if (!writingAssemblyName)
+                        {
+                            writingAssemblyName = true;
+                            builder.Append(current);
+                        }
+                        else
+                        {
+                            skippingAssemblyDetails = true;
+                        }
+                        break;
+                    default:
+                        if (!skippingAssemblyDetails)
+                        {
+                            builder.Append(current);
+                        }
+                        break;
+                }
+            }
+
+            return builder.ToString();
         }
 
         public static bool HasDefaultConstructor(Type t, bool nonPublic)
