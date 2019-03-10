589f1fb8d55a4953ecf6f22c57ac97c89755c482
xp44mm <34696643@qq.com>
2019-03-10 09:25:41 +0800

    Update ReflectionUtils.cs

--- Src/Newtonsoft.Json/Utilities/ReflectionUtils.cs
+++ Src/Newtonsoft.Json/Utilities/ReflectionUtils.cs
@@ -178,47 +178,7 @@ namespace Newtonsoft.Json.Utilities
 
         private static string RemoveAssemblyDetails(string fullyQualifiedTypeName)
         {
-            StringBuilder builder = new StringBuilder();
-
-            // loop through the type name and filter out qualified assembly details from nested type names
-            bool writingAssemblyName = false;
-            bool skippingAssemblyDetails = false;
-            for (int i = 0; i < fullyQualifiedTypeName.Length; i++)
-            {
-                char current = fullyQualifiedTypeName[i];
-                switch (current)
-                {
-                    case '[':
-                        writingAssemblyName = false;
-                        skippingAssemblyDetails = false;
-                        builder.Append(current);
-                        break;
-                    case ']':
-                        writingAssemblyName = false;
-                        skippingAssemblyDetails = false;
-                        builder.Append(current);
-                        break;
-                    case ',':
-                        if (!writingAssemblyName)
-                        {
-                            writingAssemblyName = true;
-                            builder.Append(current);
-                        }
-                        else
-                        {
-                            skippingAssemblyDetails = true;
-                        }
-                        break;
-                    default:
-                        if (!skippingAssemblyDetails)
-                        {
-                            builder.Append(current);
-                        }
-                        break;
-                }
-            }
-
-            return builder.ToString();
+                return SplitFullyQualifiedTypeName(fullyQualifiedTypeName).Value2;
         }
 
         public static bool HasDefaultConstructor(Type t, bool nonPublic)
