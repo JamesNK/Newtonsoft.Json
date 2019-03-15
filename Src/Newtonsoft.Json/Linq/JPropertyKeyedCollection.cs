56f9ea24415c40195fc92e3fa6795b88a1a36539
Damir Ainullin <damirainullin@outlook.com>
2019-03-15 20:10:28 +0300

    JToken instead of var

--- Src/Newtonsoft.Json/Linq/JPropertyKeyedCollection.cs
+++ Src/Newtonsoft.Json/Linq/JPropertyKeyedCollection.cs
@@ -129,7 +129,7 @@ namespace Newtonsoft.Json.Linq
 
             if (_dictionary != null)
             {
-                return _dictionary.TryGetValue(key, out var value) && Remove(value);
+                return _dictionary.TryGetValue(key, out JToken value) && Remove(value);
             }
 
             return false;
