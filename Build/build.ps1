a2cebb21889623ed765dfb368d955653ec6e3c82
James Newton-King <james@newtonking.com>
2021-07-24 15:56:25 +1200

    Update build.ps1

--- Build/build.ps1
+++ Build/build.ps1
@@ -14,7 +14,7 @@
   $workingName = if ($workingName) {$workingName} else {"Working"}
   $assemblyVersion = if ($assemblyVersion) {$assemblyVersion} else {$majorVersion + '.0.0'}
   $netCliChannel = "Current"
-  $netCliVersion = "5.0.200"
+  $netCliVersion = "5.0.300"
   $nugetUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
   $ensureNetCliSdk = $true
 
