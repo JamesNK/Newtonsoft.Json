abb99e96ca3843095f81692fea0d2acbf74332cf
Dylan Musil <dylanmusil@gmail.com>
2023-01-15 10:22:37 -0500

    Add test for #2775

---
+++ Src/Newtonsoft.Json.Tests/Issues/Issue2775.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Newtonsoft.Json.Linq;
+#if DNXCORE50
+using Xunit;
+using Test = Xunit.FactAttribute;
+using Assert = Newtonsoft.Json.Tests.XUnitAssert;
+using TestCase = Xunit.InlineDataAttribute;
+#else
+using NUnit.Framework;
+#endif
+
+namespace Newtonsoft.Json.Tests.Issues
+{
+    public class Issue2775
+    {
+        [Test]
+        //https://github.com/JamesNK/Newtonsoft.Json/issues/2775
+        public void TokenType()
+        {
+            var jObject = new JObject { { "NullProperty", false ? "0" : null } };
+
+            var jToken = JToken.FromObject(jObject);
+            
+            Assert.AreEqual(JTokenType.Null, jToken.Children().Children().Single().Type);
+
+            jObject = new JObject { { "NullProperty", (string)null } };
+
+            jToken = JToken.FromObject(jObject);
+            Assert.AreEqual(JTokenType.Null, jToken.Children().Children().Single().Type);
+        }
+    }
+}
