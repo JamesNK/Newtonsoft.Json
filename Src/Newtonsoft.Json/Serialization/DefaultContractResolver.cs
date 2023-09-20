01e1759cac40d8154e47ed0e11c12a9d42d2d0ff
Filip Navara <filip.navara@gmail.com>
2023-09-21 00:45:16 +0200

    Avoid LINQ expression trees on .NET 6+. (#2902)

--- Src/Newtonsoft.Json/Serialization/DefaultContractResolver.cs
+++ Src/Newtonsoft.Json/Serialization/DefaultContractResolver.cs
@@ -1412,7 +1412,7 @@ namespace Newtonsoft.Json.Serialization
             // warning - this method use to cause errors with Intellitrace. Retest in VS Ultimate after changes
             IValueProvider valueProvider;
 
-#if !(PORTABLE40 || PORTABLE || DOTNET || NETSTANDARD2_0 || NET6_0_OR_GREATER)
+#if !(PORTABLE40 || PORTABLE || DOTNET || NETSTANDARD2_0)
             if (DynamicCodeGeneration)
             {
                 valueProvider = new DynamicValueProvider(member);
