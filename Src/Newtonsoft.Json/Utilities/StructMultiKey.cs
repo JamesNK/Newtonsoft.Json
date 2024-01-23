937ba0501685c43c29f9c11ab5487fa2dcb49a33
Mahdi <git@mahdi.ninja>
2024-01-23 13:28:01 +1100

    Ensure CamelCasePropertyNamesContractResolver considers NamingStrategy in the cache

--- Src/Newtonsoft.Json/Utilities/StructMultiKey.cs
+++ Src/Newtonsoft.Json/Utilities/StructMultiKey.cs
@@ -58,4 +58,38 @@ namespace Newtonsoft.Json.Utilities
             return (Equals(Value1, other.Value1) && Equals(Value2, other.Value2));
         }
     }
+
+    internal readonly struct StructMultiKey<T1, T2, T3> : IEquatable<StructMultiKey<T1, T2, T3>>
+    {
+        public readonly T1 Value1;
+        public readonly T2 Value2;
+        public readonly T3 Value3;
+
+        public StructMultiKey(T1 v1, T2 v2, T3 v3)
+        {
+            Value1 = v1;
+            Value2 = v2;
+            Value3 = v3;
+        }
+
+        public override int GetHashCode()
+        {
+            return (Value1?.GetHashCode() ?? 0) ^ (Value2?.GetHashCode() ?? 0) ^ (Value3?.GetHashCode() ?? 0);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is StructMultiKey<T1, T2, T3> key))
+            {
+                return false;
+            }
+
+            return Equals(key);
+        }
+
+        public bool Equals(StructMultiKey<T1, T2, T3> other)
+        {
+            return (Equals(Value1, other.Value1) && Equals(Value2, other.Value2) && Equals(Value3, other.Value3));
+        }
+    }
 }
