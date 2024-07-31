94e1dbc60e98a1fed70c2ad64ca600883bfc8902
Justin Schneider <33292206+JustinSchneiderPBI@users.noreply.github.com>
2024-07-31 12:13:14 -0700

    Update Issue2887.cs to fix NewtonsoftJsonHasBuildVersion

--- Src/Newtonsoft.Json.Tests/Issues/Issue2887.cs
+++ Src/Newtonsoft.Json.Tests/Issues/Issue2887.cs
@@ -1,6 +1,29 @@
-using System.Linq;
-using Newtonsoft.Json.Linq;
-#if !DNXCORE50
+#region License
+// Copyright (c) 2007 James Newton-King
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+#if !(PORTABLE || PORTABLE40)
 using NUnit.Framework;
 #endif
 
@@ -9,7 +32,7 @@ namespace Newtonsoft.Json.Tests.Issues
     public class Issue2887
     {
 // GAC doesn't exist in NetCore apps. Issue does not repro.
-#if !PORTABLE
+#if !(PORTABLE || PORTABLE40)
         [Test]
         //https://github.com/JamesNK/Newtonsoft.Json/issues/2887
         public void LoadNewtonsoftWithGacConflict()
@@ -25,10 +48,11 @@ namespace Newtonsoft.Json.Tests.Issues
 
         [Test]
         //https://github.com/JamesNK/Newtonsoft.Json/issues/2887
-        public void NewtonsoftJsonHasPatchVersion()
+        public void NewtonsoftJsonHasBuildVersion()
         {
-            var assemblyVersion = typeof(JsonConvert).GetTypeInfo().Assembly.Version;
-            Assert.NotEqual(0, assemblyVersion.PatchVersion, "Newtonsoft needs a non-zero patch version in the assemblyversion to avoid issue 2887.");
+            var assembly = typeof(JsonConvert).GetTypeInfo().Assembly;
+            var assemblyVersion = assembly.GetName().Version;
+            Assert.AreNotEqual(0, assemblyVersion.Build, "Newtonsoft needs a non-zero build version in the assemblyversion to avoid issue 2887.");
         }
 #endif
 
