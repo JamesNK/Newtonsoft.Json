d73da92a75b0be32766bc67c33930b4a7db31fee
Samuel Sousa <samuel.sousa@doctus.com.br>
2024-10-18 17:09:13 -0300

    Related tests added

---
+++ Src/Newtonsoft.Json.Tests/Issues/Issue2954.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using Newtonsoft.Json.Linq;
+#if DNXCORE50
+using Xunit;
+using Test = Xunit.FactAttribute;
+using Assert = Newtonsoft.Json.Tests.XUnitAssert;
+using TestCase = Xunit.InlineDataAttribute;
+#else
+using NUnit.Framework;
+#endif
+
+namespace Newtonsoft.Json.Tests.Issues
+{
+    public class Issue2954
+    {
+        private const string JsonToTest = "{\"arg\": 1, \"field\": null, \"fieldEnum\": null}";
+        [Test]
+        //https://github.com/JamesNK/Newtonsoft.Json/issues/2954
+        public void Test_Ignore_Null_Values()
+        {
+            var result = JsonConvert.DeserializeObject<A>(JsonToTest, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+
+            Assert.AreEqual(result.field, 0);
+            Assert.AreEqual(result.fieldEnum, B.Option1);
+        }
+
+        [Test]
+        public void Test_Throw_JsonSerializationException()
+        {
+            ExceptionAssert.Throws<JsonSerializationException>(() => JsonConvert.DeserializeObject<A>(JsonToTest));
+        }
+
+        class A
+        {
+            public A(int arg)
+            {
+            }
+
+            public int field;
+            public B fieldEnum;
+        }
+
+        enum B
+        {
+            Option1,
+            Option2,
+        }
+    }
+}
