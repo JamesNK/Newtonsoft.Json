20d48d8c8b85071825aaee163e864cbcbf7f0bc4
Samuel Sousa <samuel.sousa@doctus.com.br>
2024-10-18 17:43:01 -0300

    Refactoring the test

--- Src/Newtonsoft.Json.Tests/Issues/Issue2954.cs
+++ Src/Newtonsoft.Json.Tests/Issues/Issue2954.cs
@@ -14,14 +14,15 @@ namespace Newtonsoft.Json.Tests.Issues
     public class Issue2954
     {
         private const string JsonToTest = "{\"arg\": 1, \"field\": null, \"fieldEnum\": null}";
+
         [Test]
         //https://github.com/JamesNK/Newtonsoft.Json/issues/2954
         public void Test_Ignore_Null_Values()
         {
             var result = JsonConvert.DeserializeObject<A>(JsonToTest, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
 
-            Assert.AreEqual(result.field, 0);
-            Assert.AreEqual(result.fieldEnum, B.Option1);
+            Assert.AreEqual(result.Field, 0);
+            Assert.AreEqual(result.FieldEnum, B.Option1);
         }
 
         [Test]
@@ -36,8 +37,8 @@ namespace Newtonsoft.Json.Tests.Issues
             {
             }
 
-            public int field;
-            public B fieldEnum;
+            public int Field { get; set; }
+            public B FieldEnum { get; set; }
         }
 
         enum B
